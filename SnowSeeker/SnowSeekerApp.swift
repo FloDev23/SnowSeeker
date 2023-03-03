427b679ae2bd958ba208da3a538ac60c0f2ff029
FloDev23 <f.fraccastoro89@gmail.com>
2023-03-03 08:32:00 +0100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Floriano Fraccastoro on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
